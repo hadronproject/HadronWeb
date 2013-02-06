89b29bc3a4610cfe5ba8cdaaf990fd8b2be98374
Burak Sezer <bburaksezer@gmail.com>
2013-02-06 23:32:52 +0200

    Fix safe filter related issues

--- sitestatic/js/main.js
+++ sitestatic/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(".news-area").delegate("h4.title a", "click", function(evt) {
         evt.preventDefault();
-        var entry_block = $(this).closest(".news-item").find("p.entry");
+        var entry_block = $(this).closest(".news-item").find("div.entry");
         if (entry_block.css("display") === "block") {
             entry_block.fadeOut(100);
         } else {
